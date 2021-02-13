e629279694fd8ac1a3d2dc37db3d7c78b8fd8803
Ratnadeep Rajendra Kharade <ratnadeepkharade@gmail.com>
2021-02-14 00:24:39 +0100

    passing attributes to element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const App = () => {
   return React.createElement(
     "div",
-    {},
+    { id: "something-important", class:"some-class" }, // attribute list for element
     React.createElement("h1", {}, "Adopt me!")
   );
 };
