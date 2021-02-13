c098598e273e17ba1d4fdf6ae92551e641ede010
Ratnadeep Rajendra Kharade <ratnadeepkharade@gmail.com>
2021-02-14 00:29:20 +0100

    reusable compoents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Havanese"),
+  ]);
+};
+
 const App = () => {
   return React.createElement(
     "div", // type of element
-    { id: "something-important", class:"some-class" }, // attribute list for element
-    React.createElement("h1", {}, "Adopt me!") // children element/s 
+    { id: "something-important", class: "some-class" }, // attribute list for element
+    [
+      React.createElement("h1", {}, "Adopt me!"), // children element/s
+      React.createElement(Pet),
+      React.createElement(Pet),
+      React.createElement(Pet),
+    ]
   );
 };
 
