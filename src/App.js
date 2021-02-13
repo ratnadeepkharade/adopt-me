91b6455ff5f2d0037e54fa4bafd0e39cb42164a2
Ratnadeep Rajendra Kharade <ratnadeepkharade@gmail.com>
2021-02-14 00:21:11 +0100

    moved js code to App.js file

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt me!")
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
