abb8b5c0a604bca5066ee0d49d302931b0ac3352
Ratnadeep Rajendra Kharade <ratnadeepkharade@gmail.com>
2021-02-14 02:07:02 +0100

    jsx composite components and expressions

--- src/App.js
+++ src/App.js
@@ -3,28 +3,39 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div", // type of element
-    { id: "something-important", class: "some-class" }, // attribute list for element
-    [
-      React.createElement("h1", {}, "Adopt me!"), // children element/s
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cocktail",
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed",
-      }),
-    ]
+  // return React.createElement(
+  //   "div", // type of element
+  //   { id: "something-important", class: "some-class" }, // attribute list for element
+  //   [
+  //     React.createElement("h1", {}, "Adopt me!"), // children element/s
+  //     React.createElement(Pet, {
+  //       name: "Luna",
+  //       animal: "Dog",
+  //       breed: "Havanese",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Pepper",
+  //       animal: "Bird",
+  //       breed: "Cocktail",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Doink",
+  //       animal: "Cat",
+  //       breed: "Mixed",
+  //     }),
+  //   ]
+  // );
+
+  return (
+    <div>
+      <h1 id="something-important" className="some-class">
+        Adopt Me!
+      </h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cocktail" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
