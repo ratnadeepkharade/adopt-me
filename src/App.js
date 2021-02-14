a175dba451d9c6496f6a3fd5551e2d7c1eb1db9d
Ratnadeep Rajendra Kharade <ratnadeepkharade@gmail.com>
2021-02-14 23:56:37 +0100

    creating search component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement(
-  //   "div", // type of element
-  //   { id: "something-important", class: "some-class" }, // attribute list for element
-  //   [
-  //     React.createElement("h1", {}, "Adopt me!"), // children element/s
-  //     React.createElement(Pet, {
-  //       name: "Luna",
-  //       animal: "Dog",
-  //       breed: "Havanese",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Pepper",
-  //       animal: "Bird",
-  //       breed: "Cocktail",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Doink",
-  //       animal: "Cat",
-  //       breed: "Mixed",
-  //     }),
-  //   ]
-  // );
-
   return (
     <div>
       <h1 id="something-important" className="some-class">
         Adopt Me!
       </h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cocktail" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
